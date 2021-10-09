484b9666a6c751ea4b23afb54d21cee197e17d50
Stefan Machmeier <stefan-machmeier@outlook.com>
2021-10-09 15:21:19 +0200

    update

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -10,7 +10,7 @@
 
   boot = {
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot.enable = pkgs.system == "x86_64-linux";
       efi.canTouchEfiVariables = true;
 
       grub = {
