5d3a015b820efdf30d3b4dd9baa9fd3fd9b87e3b
Stefan Machmeier <stefan-machmeier@outlook.com>
2021-11-30 21:18:27 +0100

    Update gnome

--- modules/workspace/gnome/default.nix
+++ modules/workspace/gnome/default.nix
@@ -1,23 +1,36 @@
 {pkgs, ...}:
 {
-    services.gnome.games.enable = false;
-    environment.systemPackages = with pkgs; [
-      # GNOME
-      gnome.gnome-tweaks
-    ];
-    environment.gnome.excludePackages = with pkgs; [ 
-      # gnome.cheese 
-      # gnome.gnome-terminal 
-      gnome-photos 
-      gnome.gedit 
-      epiphany
-      gnome.gnome-characters
-      gnome.totem
-      gnome-tour
+  services = {
+    # Enable the X11 windowing system.
+    xserver = {
+      enable = true;
+      # Enable the GNOME Desktop Environment.
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+    };
+  };
+  
+  programs.dconf.enable = true;
+  services.dbus.packages = [ pkgs.gnome3.dconf ];
+  services.udev.packages = [ pkgs.gnome3.gnome-settings-daemon ];
+  services.gnome.games.enable = false;
+  environment.systemPackages = with pkgs; [
+    # GNOME
+    gnome.gnome-tweaks
+  ];
+  environment.gnome.excludePackages = with pkgs; [ 
+    # gnome.cheese 
+    # gnome.gnome-terminal 
+    gnome-photos 
+    gnome.gedit 
+    epiphany
+    gnome.gnome-characters
+    gnome.totem
+    gnome-tour
 
-      gnome.gnome-music
-      gnome.yelp
-      gnome.gnome-maps
-      gnome.geary
-    ];
+    gnome.gnome-music
+    gnome.yelp
+    gnome.gnome-maps
+    gnome.geary
+  ];
 }
