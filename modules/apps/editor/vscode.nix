f913637516a73cbf9155988e44fc2a1eb1efecd9
Stefan Machmeier <stefan-machmeier@outlook.com>
2022-03-07 20:13:20 +0100

    Update vscode

--- modules/apps/editor/vscode.nix
+++ modules/apps/editor/vscode.nix
@@ -1,7 +1,27 @@
+{ pkgs, ...} :
 {
   home-manager.users.stefan = {
     programs.vscode = {
       enable = true;
+      package = pkgs.vscodium;    # You can skip this if you want to use the unfree version
+      extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+        
+        jnoortheen.nix-ide
+        vscodevim.vim
+        bbenoist.nix
+        ms-python.vscode-pylance
+
+        yzhang.markdown-all-in-one
+        james-yu.latex-workshop
+
+        timonwong.shellcheck
+        donjayamanne.githistory
+
+        ms-toolsai.jupyter
+        ms-azuretools.vscode-docker
+        ms-vscode.cpptools
+      ];
     };
   };
 }
