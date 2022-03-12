c731f566cbbda2cf94e0e475de548dff8e7024c4
Stefan Machmeier <stefan-machmeier@outlook.com>
2022-03-12 14:57:40 +0100

    Update vscode and i3

--- modules/apps/editor/vscode.nix
+++ modules/apps/editor/vscode.nix
@@ -8,6 +8,8 @@
         "terminal.integrated.fontFamily" = "'Source Code Pro for Powerline', 'Hack Nerd Font'";
         "workbench.colorTheme" = "Visual Studio Dark";
 
+        "latex-workshop.view.pdf.viewer" = "tab";
+
         "files.associations" = {
             "**/.i3/config" = "i3";
             "**/i3/config" = "i3";
@@ -43,6 +45,48 @@
           publisher = "redhat";
           version = "0.8.1";
           sha256 = "Vum9R2af7ERAFZ5D8AWbeOGvPbGZ/lI4ZS562T/uiM0=";
+        }
+        {
+          name = "python";
+          publisher = "ms-python";
+          version = "2022.2.1924087327";
+          sha256 = "JksPDcFpi8+JD9BDBhb6cbU0hRbNVbO/ABflOKZctWM=";
+        }
+        {
+          name = "nix-env-selector";
+          publisher = "arrterian";
+          version = "1.0.7";
+          sha256 = "DnaIXJ27bcpOrIp1hm7DcrlIzGSjo4RTJ9fD72ukKlc=";
+        }
+        {
+          name = "dotenv";
+          publisher = "mikestead";
+          version = "1.0.1";
+          sha256 = "dieCzNOIcZiTGu4Mv5zYlG7jLhaEsJR05qbzzzQ7RWc=";
+        }
+        {
+          name = "markdown-all-in-one";
+          publisher = "yzhang";
+          version = "3.4.0";
+          sha256 = "C5d2I0srdUGcmmvW2tRlMvD1RyFsUqECIQ0xLZ7ODkY=";
+        }
+        {
+          name = "markdown-preview-enhanced";
+          publisher = "shd101wyy";
+          version = "0.6.2";
+          sha256 = "tjRXAoUS63d2hsiL2a528+hQeal8pKP1eTnkf/qiUQo=";
+        }
+        {
+          name = "material-icon-theme";
+          publisher = "PKief";
+          version = "4.14.1";
+          sha256 = "OHXi0EfeyKMeFiMU5yg0aDoWds4ED0lb+l6T12XZ3LQ=";
+        }
+        {
+          name = "nixpkgs-fmt";
+          publisher = "B4dM4n";
+          version = "0.0.1";
+          sha256 = "vz2kU36B1xkLci2QwLpl/SBEhfSWltIDJ1r7SorHcr8=";
         }
       ];
     };
