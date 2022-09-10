98051d5528f51c19a85b9b381254131e9749802c
Stefan Machmeier <stefan-machmeier@outlook.com>
2022-09-10 16:24:02 +0200

    Fix virtualbox

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -4,7 +4,12 @@
     docker-compose
   ];
   virtualisation = {
-    virtualbox.host.enable = true;
+    virtualbox = {
+      host = {
+        enable = true;
+        enableExtensionPack = true;
+      };
+    };
     docker = {
       enable = true;
 
@@ -13,6 +18,13 @@
         dates = "weekly";
       };
     };
+    # Enable a daemon that manages virtual machines.
+    # Docs: https://wiki.archlinux.org/index.php/libvirt
+    libvirtd.enable = true;
+
+    # Allow unprivileged user to pass USB devices connected to
+    # this machine to libvirt VMs, both local and remote
+    # spiceUSBRedirection.enable = true;
   };
   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
 }
