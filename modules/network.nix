5f47a0c4b7b0353136d30a6bd59ee4bacaea017e
Stefan Machmeier <stefan-machmeier@outlook.com>
2022-11-23 18:10:30 +0100

    Update network settings

--- modules/network.nix
+++ modules/network.nix
@@ -4,11 +4,14 @@
     networkmanager = {
       enable = true;
       dns = "systemd-resolved";
+      # insertNameservers = [ "xxx.xxx.xxx.xxx" ];
     };
-    # nameservers = [ "XXX.XXX.XXX.XXX" ];
+    # nameservers = [ "xxx.xxx.xxx.xxx" ];
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
     # Per-interface useDHCP will be mandatory in the future, so this generated config
     # replicates the default behaviour.
     useDHCP = false;
   };
 }
+
+# sudo systemd-resolve --set-dns 129.206.7.220 --interface enp2s0f0
